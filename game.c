ab54c3663dda0f44e6dd7502b0d5b550bf4c2ceb
Anshuman Sharma <73778637+anshyyy@users.noreply.github.com>
2021-01-24 15:15:23 +0530

    Add files via upload

---
+++ game.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+     int number, guess , nguesses=1;
+     srand(time(0));
+     number= rand()%100+1;
+     do
+     {
+        printf("Enter your number between 1 to 100\n");
+         scanf("%d" , &guess);
+         if (guess>number)
+        
+         {
+             printf("lower number please\n");
+         } 
+         else if (guess<number)
+         {
+             printf("higher number please\n");
+         }
+         else
+         {
+             printf("you guessed it in %d attempts \n", nguesses);
+         }
+         
+         nguesses++;
+     } while (guess!=number);
+     
+
+     return 0;
+}
